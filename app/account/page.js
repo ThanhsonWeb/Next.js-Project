1e6c4b4921a603d5553185ebdbdae13d6b34d6d8
ThanhsonWeb <sondeptroi@gmail.com>
2026-06-01 15:21:53 +0700

    Sett up Nextjs project

---
+++ app/account/page.js
@@ -0,0 +1,9 @@
+function page() {
+	return (
+		<div>
+			<h1>Account</h1>
+		</div>
+	);
+}
+
+export default page;
---
+++ app/cabins/page.js
@@ -0,0 +1,9 @@
+function page() {
+	return (
+		<div>
+			<p>Cabin </p>
+		</div>
+	);
+}
+
+export default page;
