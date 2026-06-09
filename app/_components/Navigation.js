81939a0b0519da2055a8ab6a412eab9a514d17d0
ThanhsonWeb <sondeptroi@gmail.com>
2026-06-09 09:35:22 +0700

    Getting the user session (display image in  navigation)

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
+import Image from "next/image";
 
-export default function Navigation() {
+export default async function Navigation() {
+	const session = await auth();
+	console.log(session);
+
 	return (
 		<nav className="z-10 text-xl">
 			<ul className="flex gap-16 items-center">
@@ -20,13 +25,29 @@ export default function Navigation() {
 						About
 					</Link>
 				</li>
+				{/* display it here */}
 				<li>
-					<Link
-						href="/account"
-						className="hover:text-accent-400 transition-colors"
-					>
-						Guest area
-					</Link>
+					{session?.user?.image ? (
+						<Link
+							href="/account"
+							className="hover:text-accent-400 transition-colors flex items-center gap-5  "
+						>
+							<img
+								src={session.user.image}
+								alt={session.user.name}
+								referrerPolicy="no-referrer"
+								className="h-8 rounded-full"
+							/>
+							<span>Guest area</span>
+						</Link>
+					) : (
+						<Link
+							href="/account"
+							className="hover:text-accent-400 transition-colors"
+						>
+							Guest area
+						</Link>
+					)}
 				</li>
 			</ul>
 		</nav>
