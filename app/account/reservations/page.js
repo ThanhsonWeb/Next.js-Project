a48a62bf1ff625339e7cc2ef3c391d49e05f2c8d
ThanhsonWeb <sondeptroi@gmail.com>
2026-06-12 11:14:51 +0700

    Building a guest reservation page

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,25 +1,32 @@
 import ReservationCard from "@/app/_components/ReservationCard";
+import { auth } from "@/app/_lib/auth";
+import { getBookings } from "@/app/_lib/data-service";
+import Link from "next/link";
 
 export const metadata = {
 	title: "Reservations",
 };
 
-export default function Page() {
-	// CHANGE
-	const bookings = [];
+export default async function Page() {
+	const session = await auth();
+	// b1:  get  bookings from exact current guest (by id)
+	const bookings = await getBookings(session.user.guestId); //6
 
+	// b2 in supabase -> bookings table -> two rows have guestId is 6
+	
 	return (
 		<div>
 			<h2 className="font-semibold text-2xl text-accent-400 mb-7">
 				Your reservations
 			</h2>
 
+			{/* b3 render those 2 bookings  */}
 			{bookings.length === 0 ? (
 				<p className="text-lg">
 					You have no reservations yet. Check out our s
-					<a className="underline text-accent-500" href="/cabins">
+					<Link className="underline text-accent-500" href="/cabins">
 						luxury cabins &rarr;
-					</a>
+					</Link>
 				</p>
 			) : (
 				<ul className="space-y-6">
